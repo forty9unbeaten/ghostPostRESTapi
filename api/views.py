65ab121c0852ec2f7a06b038e94f2d82abd05412
Rob Spears <rob.sp3ars@gmail.com>
2020-06-24 22:48:15 -0400

    Add PostsViewSet that return JSON Post model instance data

--- api/views.py
+++ api/views.py
@@ -1,1 +1,27 @@
-from django.shortcuts import render
+from api.models import Post
+from api.serializers import PostSerializer
+from rest_framework.decorators import action
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+
+class PostsViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    @action(detail=True, methods=['post'])
+    def upvote(self, request, pk='id'):
+        post = self.get_object()
+        post.upvotes += 1
+        post.save()
+        serializer = PostSerializer(instance=post)
+        return Response(data=serializer.data)
+
+    @action(detail=True, methods=['post'])
+    def downvote(self, request, pk='id'):
+        post = self.get_object()
+        post.downvotes += 1
+        post.save()
+        serializer = PostSerializer(instance=post)
+        return Response(data=serializer.data)
